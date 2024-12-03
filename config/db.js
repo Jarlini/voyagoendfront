ee3e6ff607151d3207e7f4919a320cc4ad0cbcb9
jarlini <jalujalu998@gmail.com>
2024-12-04 04:59:47 +0530

    first commilliiiliit

--- config/db.js
+++ config/db.js
@@ -4,16 +4,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch(err => {
+      console.error('Database connection error:', err.message);
+      console.log('Retrying connection in 5 seconds...');
+      setTimeout(connectWithRetry, 5000);
     });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
 };
 
 module.exports = connectDB;
